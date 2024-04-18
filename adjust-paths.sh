4e779fe5cdc6665649b4faa12c9f68241a461bb8
raphael-rouf <166538432+raphael-rouf@users.noreply.github.com>
2024-04-18 12:02:59 -0400

    Delete adjust-paths.sh

--- adjust-paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Adjust paths in files
-sed -i 's|/assets/|/my-portfolio/assets/|g' $(find . -type f -name "*.html")
-sed -i 's|/assets/|/my-portfolio/assets/|g' $(find . -type f -name "*.css")
-sed -i 's|/assets/|/my-portfolio/assets/|g' $(find . -type f -name "*.ts")
-
-# Build your Angular project
-ng build --configuration=production --base-href=/my-portfolio/
