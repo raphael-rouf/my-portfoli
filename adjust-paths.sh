5678ea52d3f0f728a9e5e3648a33c04efbc68b86
raphael-rouf <roufraphael@gmail.com>
2024-04-18 18:29:13 +0000

    Deploy to gh-pages Pages

--- adjust-paths.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Adjust paths in files
-sed -i 's|/assets/|/my-portfolio/assets/|g' $(find . -type f -name "*.css")
-sed -i 's|/assets/|/my-portfolio/assets/|g' $(find . -type f -name "*.html")
