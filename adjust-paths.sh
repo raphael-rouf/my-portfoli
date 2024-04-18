f008723ae4ae2e3dd9c8902026e599e22f0bbf44
raphael-rouf <166538432+raphael-rouf@users.noreply.github.com>
2024-04-18 12:06:30 -0400

    Create adjust-paths.sh

---
+++ adjust-paths.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Adjust paths in files
+sed -i 's|assets/|my-portfolio/assets/|g' $(find . -type f -name "*.html")
+sed -i 's|assets/|my-portfolio/assets/|g' $(find . -type f -name "*.css")
+sed -i 's|assets/|my-portfolio/assets/|g' $(find . -type f -name "*.ts")
